dc5d606f41f2a29c38eff65deceb19336b95d9c7
Francesco Bartoli <xbartolone@gmail.com>
2024-03-03 21:32:00 +0100

    Add schemathesis tests to respect the OpenAPI contract

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,12 +4,6 @@ from cli import app
 from typer.testing import CliRunner
 
 
-@pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
-
-
 def test_openapi_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(app, ["openapi"])
