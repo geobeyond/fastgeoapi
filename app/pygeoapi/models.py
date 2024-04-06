1c75175e0a5bc85dc02e208258d5d1013f209781
Francesco Bartoli <xbartolone@gmail.com>
2024-04-06 21:38:41 +0200

    Fix missing not found response for options method
    Fix missing not found response for options method

---
+++ app/pygeoapi/models.py
@@ -0,0 +1,11 @@
+"""pygeoapi models module."""
+
+from openapi_pydantic.v3.v3_0_3 import Reference
+
+not_found = {
+    "404": Reference(
+        **{
+            "$ref": "https://schemas.opengis.net/ogcapi/features/part1/1.0/openapi/ogcapi-features-1.yaml#/components/responses/NotFound"  # noqa
+        }
+    )
+}
