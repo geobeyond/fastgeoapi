190c6dfd2b66afc27958cd2cc81ace2b034575a3
Francesco Bartoli <xbartolone@gmail.com>
2024-04-06 17:46:30 +0200

    Add unauthorized response to paths

---
+++ app/auth/models.py
@@ -0,0 +1,9 @@
+from openapi_pydantic.v3.v3_0_3 import Response
+
+
+unauthorized = {
+    "401": Response(
+        description="Unauthorized response",
+        message="Unauthenticated"
+    )
+}
