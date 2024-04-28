8c2dbe48766724468a235127c9db96d313a2ea5e
Francesco Bartoli <xbartolone@gmail.com>
2024-04-28 02:24:17 +0200

    Fix failing test for newlines and carriage returns
    Fix failing test for newlines and carriage returns

--- tests/test_openapi_contract.py
+++ tests/test_openapi_contract.py
@@ -8,6 +8,15 @@ schema = schemathesis.from_pytest_fixture("protected_apikey_schema")
 @schema.parametrize()
 def test_api(case):
     """Test the API with API-KEY protection."""
+    if case.path_parameters:
+        if case.path_parameters.get("jobId"):
+            job_id = case.path_parameters.get("jobId")
+            if r"\n" or r"\r" in job_id:
+                case.path_parameters["jobId"] = job_id.strip()
+            if "%0A" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0A", "")
+            if "%0D" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0D", "")
     case.headers = {"X-API-KEY": "pygeoapi"}
     response = case.call()
     case.validate_response(response)
