6075da3da165df5d97721912191ae7ed57517b32
Francesco Bartoli <xbartolone@gmail.com>
2024-04-28 18:24:10 +0200

    Add support for different algorithms beyond RS256

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,7 +1,22 @@
 """Authentication models module."""
 
+import typing
+
+import pydantic
 from openapi_pydantic.v3.v3_0_3 import Response
 
 unauthorized = {
     "401": Response(description="Unauthorized response", message="Unauthenticated")
 }
+
+
+class OAuth2Claim(pydantic.BaseModel):
+    """Parse OAuth2 claims."""
+
+    jti: str = pydantic.Field(...)
+    sub: str = pydantic.Field(...)
+    iat: int = pydantic.Field(...)
+    exp: int = pydantic.Field(...)
+    iss: str = pydantic.Field(...)
+    aud: str = pydantic.Field(...)
+    client_id: typing.Optional[str] = pydantic.Field(None)
