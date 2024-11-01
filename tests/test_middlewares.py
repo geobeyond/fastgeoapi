770e340749a6ba632172216c0810be531183c6e5
Francesco Bartoli <xbartolone@gmail.com>
2024-11-01 15:35:00 +0100

    Fix pre-commit

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -8,22 +8,24 @@ from app.config.app import configuration as cfg
 
 @pytest.mark.asyncio
 async def test_pygeoapi_links_behind_proxy(
-    create_app_with_reverse_proxy_enabled
+    create_app_with_reverse_proxy_enabled,
 ) -> None:
+    """Test presence of reverse proxy base urls in links."""
     app = create_app_with_reverse_proxy_enabled()
-    async with AsyncClient(app=app) as client:
-        PROTO = "https"
-        HOST = "proxy.example.com"
+
+    async with AsyncClient(app=app, timeout=30) as client:
+        _proto = "https"
+        _host = "proxy.example.com"
         response = await client.get(
             f"{cfg.APP_URI}{cfg.FASTGEOAPI_CONTEXT}/collections",
             headers={
-                "X-Forwarded-Proto": PROTO,
-                "X-Forwarded-Host": HOST,
+                "X-Forwarded-Proto": _proto,
+                "X-Forwarded-Host": _host,
             },
         )
         assert response.status_code == 200
         links = response.json()["links"]
         for link in links:
             href = link["href"]
-            assert PROTO in href
-            assert HOST in href
+            assert _proto in href
+            assert _host in href
