27e8a167316db482799d71fcd84e254a819486e9
Francesco Bartoli <xbartolone@gmail.com>
2025-10-13 01:13:51 +0200

    Fix schemathesis on github action

--- tests/test_openapi_contract.py
+++ tests/test_openapi_contract.py
@@ -2,39 +2,39 @@
 
 import schemathesis
 
-schema_apikey = schemathesis. == schemathesisschemathesis.pytest.from_fixture('protected_bearer_schema')from_pytest_fixture("")
+schema_apikey = schemathesis.from_pytest_fixture("protected_apikey_schema")
 schema_bearer = schemathesis.from_pytest_fixture("protected_bearer_schema")
 
 
-@schema_apikeyschema_apikey.parametrizeparametrize()
+@schema_apikey.parametrize()
 def test_api_with_apikey(case):
     """Test the API with API-KEY protection."""
     if case.path_parameters:
-        if case.path_parameters.get('jobId'):
-            job_id = case.path_parameters.get('jobId')
-            if r'\n' or r'\r' in job_id:
-                case.path_parameters['jobId'] = job_id.strip()
-            if '%0A' in job_id:
-                case.path_parameters['jobId'] = job_id.replace('%0A', '')
-            if '%0D' in job_id:
-                case.path_parameters['jobId'] = job_id.replace('%0D', '')
-    case.headers = {'X-API-KEY': 'pygeoapi'}
+        if case.path_parameters.get("jobId"):
+            job_id = case.path_parameters.get("jobId")
+            if r"\n" or r"\r" in job_id:
+                case.path_parameters["jobId"] = job_id.strip()
+            if "%0A" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0A", "")
+            if "%0D" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0D", "")
+    case.headers = {"X-API-KEY": "pygeoapi"}
     response = case.call()
     case.validate_response(response)
 
 
-@schema_bearer@schema_bearer.parametrize()
+@schema_bearer.parametrize()
 def test_api_with_bearer(case, access_token):
     """Test the API with Authorization Bearer token protection."""
     if case.path_parameters:
-        if case.path_parameters.get('jobId'):
-            job_id = case.path_parameters.get('jobId')
-            if r'\n' or r'\r' in job_id:
-                case.path_parameters['jobId'] = job_id.strip()
-            if '%0A' in job_id:
-                case.path_parameters['jobId'] = job_id.replace('%0A', '')
-            if '%0D' in job_id:
-                case.path_parameters['jobId'] = job_id.replace('%0D', '')
-    case.headers = {'Authorization': f'Bearer {access_token}'}
+        if case.path_parameters.get("jobId"):
+            job_id = case.path_parameters.get("jobId")
+            if r"\n" or r"\r" in job_id:
+                case.path_parameters["jobId"] = job_id.strip()
+            if "%0A" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0A", "")
+            if "%0D" in job_id:
+                case.path_parameters["jobId"] = job_id.replace("%0D", "")
+    case.headers = {"Authorization": f"Bearer {access_token}"}
     response = case.call()
     case.validate_response(response)
